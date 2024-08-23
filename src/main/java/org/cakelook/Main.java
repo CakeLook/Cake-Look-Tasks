734b895184329ab042f3e1ec568df2f9033f5e0c
Max <maxmiliandirer2007@gmail.com>
2024-08-23 19:25:47 +0200

    Kalkulačka - Max

--- src/main/java/org/cakelook/Main.java
+++ src/main/java/org/cakelook/Main.java
@@ -1,7 +1,26 @@
 package org.cakelook;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What number you want: ");
+        double knedlik = scanner.nextDouble();
+        System.out.println("Second number monkey: ");
+        double kvin = scanner.nextDouble();
+
+        System.out.println("Soucet: " + (knedlik + kvin));
+        System.out.println("Rozdil: " + (knedlik - kvin));
+        System.out.println("Soucin: " + (knedlik * kvin));
+        System.out.println("Podil " + (knedlik / kvin));
 
     }
 }
+
+// Scanner (v podstatě hledá user input)
+// import (scanner jsem musel první importnout)
+// Non-static method 'nextDouble()' cannot be referenced from a static context (potřeboval jsem tam přidat nějakou class ale ta se nemuže jmneovat stejně jako fuknce, takže jsem tam dal Scanner akorat maly S)
+// next.Double (v podstatě to jsou dvě číselný hodnoty)
+// 👹👹👹👹👹👹👹👹👹👹👹👹👹👹👹👹👹👹👹👹👹👹👹👹👹👹👹👹👹👹👹👹
