1202c6c97ed0c3ed243a9bb7e366eceaedcd8e78
@pantoflicekj <pantoflicekj@ssemi.cz>
2024-09-13 16:44:37 +0200

    Cykly jachym task done

---
+++ src/main/java/org/cakelook/cykly/cyklyMain.java
@@ -0,0 +1,11 @@
+package org.cakelook.cykly;
+
+public class cyklyMain {
+    public static void main(String[] args) {
+        var number = 100;
+        for (int i = 0; i <= number; i++) {
+
+            System.out.println(i);
+        }
+    }
+}
